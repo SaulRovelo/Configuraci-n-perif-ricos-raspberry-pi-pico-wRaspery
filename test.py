e510b269d47075575e49bea889d146cda44bad55
Saul Rovelo <saul.rovelo@cua.uam.mx>
2024-05-08 13:31:49 -0600

    Esta lista la prueba code digital clock test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 
 from digital_clock import digital_clock
-import time
+from time import sleep
 
 def clock_test()-> None:
     clock= digital_clock()
+    while True:
+        h,m,s = clock.get_time()
+        time = f'{h:02 } : {m:02} : {s:02}:'
+        print(time)
+        sleep(1)
+        clock.incremet()
+        pass
 
